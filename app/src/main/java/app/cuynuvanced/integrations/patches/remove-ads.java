517b8881fbe405afc481ac4fee491daa7f551ebb
Cuynu <kongu2k@gmail.com>
2023-02-22 00:49:05 +0700

    Create remove-ads.java

---
+++ app/src/main/java/app/cuynuvanced/integrations/patches/remove-ads.java
@@ -0,0 +1,7 @@
+package com.cuynuvanced.patches.removeads;
+
+import com.cuynuvanced.integrations.settings.SettingsEnum
+
+public class remove-ads {
+   public static boolean remove-ads() {
+          return !
