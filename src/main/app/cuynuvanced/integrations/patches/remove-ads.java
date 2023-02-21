eda926bb9b351fe8a7169db25522721bd9ec2e3e
Cuynu <kongu2k@gmail.com>
2023-02-22 00:47:53 +0700

    Delete src/main/app/cuynuvanced/integrations/patches directory

--- src/main/app/cuynuvanced/integrations/patches/remove-ads.java
+++
@@ -1,7 +0,0 @@
-package com.cuynuvanced.patches.removeads;
-
-import com.cuynuvanced.integrations.settings.SettingsEnum
-
-public class remove-ads {
-   public static boolean remove-ads() {
-          return !
